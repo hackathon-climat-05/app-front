a48c9ba3ecb47d538a51d02013f801f844c5ed64
Duc-Thomas <duckiduc.dev@gmail.com>
2022-06-30 02:32:59 +0200

    Add Header + Login
    Co-authored-by: Lou-AnnBoinet <Lou-AnnBoinet@users.noreply.github.com>

---
+++ pages/login.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from '../components/Header'
+import styles from '../styles/Home.module.css'
+
+const Login: NextPage = () => {
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
+
+export default Login
